c1f6b3c3742efba25953235b6c4a36c1794bf7d1
Ilya Shcherbakov <86749581+LikeKugi@users.noreply.github.com>
2023-06-20 20:14:41 +0300

    remove useless import

--- src/components/StateBlock/StateBlock.tsx
+++ src/components/StateBlock/StateBlock.tsx
@@ -1,7 +1,6 @@
-import React, {FC, JSX, useEffect, useState} from "react";
+import React, {FC, JSX} from "react";
 import styles from './StateBlock.module.css';
 import canvasState from "../../store/canvasState";
-import toolState from "../../store/toolState";
 import {observer} from "mobx-react-lite";
 
 
@@ -15,11 +14,6 @@ const StateBlock: FC<IStateBlockProps> = observer(({openModal, store}): JSX.Elem
     openModal()
   }
 
-  const [lw, setLW] = useState(toolState.tool?.ctx?.lineWidth);
-  useEffect(() => {
-    setLW(toolState.tool?.ctx?.lineWidth || 1)
-  }, [toolState.tool?.ctx?.lineWidth, toolState.tool?.ctx, toolState.tool, toolState.lineWidth])
-
   return (
     <aside className={styles.state}>
       <button className={styles.state__btn}
@@ -27,10 +21,6 @@ const StateBlock: FC<IStateBlockProps> = observer(({openModal, store}): JSX.Elem
       </button>
       <div>
         <h3 className={styles.state__title}>{canvasState.username}</h3>
-        <p>{store.lineWidth}</p>
-        <p>Tool State: {toolState.lineWidth}</p>
-        <p>LW in state: {lw}</p>
-        <p>LW: {toolState.tool?.ctx?.lineWidth}</p>
       </div>
 
     </aside>
